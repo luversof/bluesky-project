ffce1a14eca8283d54e03efb008095a11037a2c1
luversof <luversof@hanmail.net>
2013-06-28 05:37:11 +0900

    web 설정 추가

---
+++ parent/webapp-config/webapp-config-core/src/main/java/net/luversof/web/core/config/WebMvcConfig.java
@@ -0,0 +1,200 @@
+package net.luversof.web.core.config;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Properties;
+
+import nz.net.ultraq.thymeleaf.LayoutDialect;
+
+import org.apache.commons.lang3.ArrayUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.accept.ContentNegotiationManager;
+import org.springframework.web.accept.ContentNegotiationStrategy;
+import org.springframework.web.accept.HeaderContentNegotiationStrategy;
+import org.springframework.web.accept.PathExtensionContentNegotiationStrategy;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.handler.SimpleMappingExceptionResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
+import org.springframework.web.servlet.view.json.MappingJacksonJsonView;
+import org.thymeleaf.spring3.SpringTemplateEngine;
+import org.thymeleaf.spring3.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "net.luversof",useDefaultFilters=false,includeFilters=@Filter(type=FilterType.ANNOTATION, value={ Controller.class, ControllerAdvice.class }))
+//@PropertySource(name="mvcProps", value="classpath:props/mvc.properties")
+@EnableAspectJAutoProxy
+
+public class WebMvcConfig  extends WebMvcConfigurerAdapter {
+
+	@Autowired
+	private ApplicationContext applicationContext;
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("index");
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resources/**").addResourceLocations("/public-resources/");
+		registry.addResourceHandler("/css/**").addResourceLocations("/public-resources/css/");
+		registry.addResourceHandler("/js/**").addResourceLocations("/public-resources/js/");
+		registry.addResourceHandler("/img/**").addResourceLocations("/public-resources/img/").setCachePeriod(31556926);
+	}
+
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+
+
+//	@Bean
+//	public LoginCheckInterceptor loginCheckInterceptor() {
+//		return new LoginCheckInterceptor();
+//	}
+//	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new LocaleChangeInterceptor());
+
+		super.addInterceptors(registry);
+	}
+
+	@Bean
+	public SessionLocaleResolver localeResolver() {
+		SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
+		sessionLocaleResolver.setDefaultLocale(Locale.KOREA);
+		return sessionLocaleResolver;
+	}
+
+	@Bean
+	public LocaleChangeInterceptor localeChangeInterceptor() {
+		return new LocaleChangeInterceptor();
+	}
+
+	@Bean
+	public SimpleMappingExceptionResolver simpleMappingExceptionResolver () {
+		SimpleMappingExceptionResolver simpleMappingExceptionResolver = new SimpleMappingExceptionResolver();
+		simpleMappingExceptionResolver.setDefaultErrorView("error/error");
+		Properties mappings = new Properties();
+		mappings.put("net.luversof.core.exception.GenericException", "error/error");
+		mappings.put("net.luversof.core.exception.UnauthorizedException", "error/error");
+		simpleMappingExceptionResolver.setExceptionMappings(mappings);
+
+		return simpleMappingExceptionResolver;
+	}
+
+
+	@Bean
+	public ContentNegotiatingViewResolver contentNegotiatingViewResolver() {
+		ContentNegotiatingViewResolver viewResolver = new ContentNegotiatingViewResolver();
+
+		// 확장자 기준 결정
+		Map<String, MediaType> mediaTypes = new HashMap<String, MediaType>();
+		mediaTypes.put("html", MediaType.TEXT_HTML);
+		mediaTypes.put("json", MediaType.APPLICATION_JSON);
+		ContentNegotiationStrategy pathExtensionContentNegotiationStrategy = new PathExtensionContentNegotiationStrategy(mediaTypes);
+
+		// 헤더값 기준 결정
+		ContentNegotiationStrategy  headerContentNegotiationStrategy = new HeaderContentNegotiationStrategy();
+
+		/* order에 따라 우선순위 결정되므로 순서 주의 */
+		ContentNegotiationManager contentNegotiationManager = new ContentNegotiationManager(pathExtensionContentNegotiationStrategy, headerContentNegotiationStrategy);
+		viewResolver.setContentNegotiationManager(contentNegotiationManager);
+
+
+		 /*contentNegotiationManagerFactoryBean의 mediaType를 사용한 방법*/
+
+		// ContentNegotiationManagerFactoryBean
+		// contentNegotiationManagerFactoryBean = new
+		// ContentNegotiationManagerFactoryBean();
+		// Properties mediaTypes2 = new Properties();
+		// mediaTypes2.put("html", "text/html");
+		// mediaTypes2.put("json", "application/json");
+		// contentNegotiationManagerFactoryBean.setMediaTypes(mediaTypes2);
+		// try {
+		// viewResolver.setContentNegotiationManager(contentNegotiationManagerFactoryBean.getObject());
+		// } catch (Exception e) {
+		// e.printStackTrace();
+		// }
+
+		List<ViewResolver> viewResolvers = new ArrayList<ViewResolver>();
+
+		ThymeleafViewResolver thymeleafViewResolver = new ThymeleafViewResolver();
+		thymeleafViewResolver.setTemplateEngine(templateEngine());
+		thymeleafViewResolver.setCharacterEncoding("UTF-8");
+
+		viewResolvers.add(thymeleafViewResolver);
+		viewResolver.setViewResolvers(viewResolvers);
+
+
+		List<View> defaultViews = new ArrayList<View>();
+		defaultViews.add(new MappingJacksonJsonView());
+
+		viewResolver.setDefaultViews(defaultViews);
+		return viewResolver;
+	}
+
+	@Bean
+	public ServletContextTemplateResolver templateResolver() {
+		ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver();
+		templateResolver.setPrefix("/WEB-INF/views/");
+		templateResolver.setSuffix(".html");
+		templateResolver.setTemplateMode("HTML5");
+		// templateResolver.setCharacterEncoding("UTF-8");
+		// 개발시엔 false로 설정하자.
+		templateResolver.setCacheable(true);
+		String[] profiles = applicationContext.getEnvironment().getActiveProfiles();
+
+		if (ArrayUtils.contains(profiles, "live")) {
+			templateResolver.setCacheable(true);
+		} else {
+			templateResolver.setCacheable(false);
+		}
+		return templateResolver;
+	}
+
+	@Bean
+	public SpringTemplateEngine templateEngine() {
+		SpringTemplateEngine springTemplateEngine = new SpringTemplateEngine();
+		springTemplateEngine.setTemplateResolver(templateResolver());
+		springTemplateEngine.addDialect(new LayoutDialect());	// thymeleaf-layout-dialect 사용 설정
+		return springTemplateEngine;
+	}
+
+	// @Bean
+	// public ThymeleafViewResolver thymeleafViewResolver() {
+	// ThymeleafViewResolver thymeleafViewResolver = new
+	// ThymeleafViewResolver();
+	// thymeleafViewResolver.setTemplateEngine(templateEngine());
+	// thymeleafViewResolver.setCharacterEncoding("UTF-8");
+	// // thymeleafViewResolver.setCache(false);
+	// return thymeleafViewResolver;
+	// }
+
+}
---
+++ parent/webapp/webapp-bluesky/src/main/java/net/luversof/web/config/DispatcherServletInitializer.java
@@ -0,0 +1,37 @@
+package net.luversof.web.config;
+
+import javax.servlet.Filter;
+
+import net.luversof.core.config.AppConfig;
+import net.luversof.web.core.config.WebMvcConfig;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class DispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class[] { AppConfig.class };
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class[] { WebMvcConfig.class };
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
+	}
+
+	@Override
+	protected Filter[] getServletFilters() {
+		CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+		characterEncodingFilter.setEncoding("UTF-8");
+		characterEncodingFilter.setForceEncoding(true);
+		return new Filter[] { new HiddenHttpMethodFilter(), characterEncodingFilter };
+	}
+
+}
