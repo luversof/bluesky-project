0fdadf8ef2795f373342ae17f71d3aafcdb4c5d8
luversof <luversof@hanmail.net>
2013-09-05 16:04:19 +0900

    Google Analytics 적

---
+++ parent/webapp/webapp-bluesky/src/test/java/net/luversof/web/TestClass.java
@@ -0,0 +1,29 @@
+package net.luversof.web;
+
+import lombok.extern.slf4j.Slf4j;
+import net.luversof.core.config.AppConfig;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@Slf4j
+@WebAppConfiguration
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes={AppConfig.class})
+public class TestClass {
+	
+	@Autowired
+	private ApplicationContext ctx;
+
+	@Test
+	@Ignore
+	public void test(){
+		log.debug("ctx : {}", ctx);
+	}
+}
