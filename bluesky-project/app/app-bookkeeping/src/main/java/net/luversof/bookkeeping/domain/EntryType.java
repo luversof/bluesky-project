145fd283696b6966ec1242245165ea1546e9cb24
luversof <luversof@hanmail.net>
2013-09-24 00:21:32 +0900

    .

--- bluesky-project/app/app-bookkeeping/src/main/java/net/luversof/bookkeeping/domain/EntryType.java
+++ bluesky-project/app/app-bookkeeping/src/main/java/net/luversof/bookkeeping/domain/EntryType.java
@@ -1,6 +1,5 @@
 package net.luversof.bookkeeping.domain;
 
-import lombok.Getter;
 
 /**
  * 기록 유형
@@ -8,13 +7,6 @@ import lombok.Getter;
  *
  */
 public enum EntryType {
-	DEBIT(1),	//인출
-	CREDIT(2);	//입금
-	
-	@Getter
-	private long id;
-	
-	private EntryType(long id) {
-		this.id = id;
-	}
+	DEBIT,	//인출
+	CREDIT;	//입금
 }
--- bluesky-project/webapp/webapp-bluesky/src/main/java/net/luversof/web/AuthorizeRole.java
+++ bluesky-project/webapp/webapp-bluesky/src/main/java/net/luversof/web/AuthorizeRole.java
@@ -1,5 +1,9 @@
 package net.luversof.web;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access=AccessLevel.PRIVATE)
 public class AuthorizeRole {
-	public final static String PRE_AUTHORIZE_ROLE = "hasRole('ROLE_USER')";
+	public static final String PRE_AUTHORIZE_ROLE = "hasRole('ROLE_USER')";
 }
