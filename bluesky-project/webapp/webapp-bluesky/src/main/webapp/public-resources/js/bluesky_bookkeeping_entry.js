f79c0b28ff8d4690b36db9171bd4d942439c5c57
luversof <luversof@hanmail.net>
2013-10-14 02:01:22 +0900

    .

--- bluesky-project/webapp/webapp-bluesky/src/main/webapp/public-resources/js/bluesky_bookkeeping_entry.js
+++ bluesky-project/webapp/webapp-bluesky/src/main/webapp/public-resources/js/bluesky_bookkeeping_entry.js
@@ -26,42 +26,106 @@ $(document).ready(function() {
 		dataKey : "model",
 		view : view,
 		initialize : function() {
-			console.log("우하하하하");
+			$(".entry-menu-add").css("cursor", "pointer");
 		},
 		events : {
 			"tr" : {
-				"mouseover" : "menuShow",
+				"mouseover" : ["menuShow", "menuResetDisplayCheck"],
 				"mouseleave" : "menuHide"
-			}
+			},
+			"[contenteditable=true]" : { "keyup" : "menuResetDisplayCheck", "focusin" : "menuResetDisplayCheck"},
+			"span.glyphicon-edit" : { "click" : "modify" },
+			"span.glyphicon-remove" : { "click" : "remove" },
+			"span.glyphicon-refresh" : { "click" : "reset" }
+		},
+		externalEvents : {
+			".entry-menu-add" : { "click" : "meunAddDisplay" },
+			".entry-menu-add .entry-add" : { "click" : "add" }
 		},
 		menuShow : function(event) {
-			console.log("[controller] menuShow");
-			$(event.currentTarget).find("td:last").empty().append(
-					$("<span>").addClass("glyphicon glyphicon-edit").attr("title", "edit").css("cursor", "pointer").tooltip()
-				).append(" ").append(
-					$("<span>").addClass("glyphicon glyphicon-remove").attr("title", "remove").css("cursor", "pointer").tooltip()
-				).append(" ").append(
-					$("<span>").addClass("glyphicon glyphicon-refresh").attr("title", "reset").css("cursor", "pointer").tooltip().hide()
-				);
-			event.data.controller.menuResetDisplayCheck($(event.currentTarget).find(".glyphicon-refresh"));
+			console.debug("[controller] menuShow");
+			if (!$(event.currentTarget).closest("[data-id]").find("td:last").html() == "") {
+				return;
+			}
+			$(event.currentTarget).closest("[data-id]").find("td:last").empty().append(
+				$("<span>").addClass("glyphicon glyphicon-edit").attr("title", "edit").css("cursor", "pointer").tooltip()
+			).append(" ").append(
+				$("<span>").addClass("glyphicon glyphicon-remove").attr("title", "remove").css("cursor", "pointer").tooltip()
+			).append(" ").append(
+				$("<span>").addClass("glyphicon glyphicon-refresh").attr("title", "reset").css("cursor", "pointer").tooltip().hide()
+			);
 		},
 		menuHide : function(event) {
+			console.debug("[controller] menuHide");
 			$(event.currentTarget).find("td:last").empty();
 		},
-		menuResetDisplayCheck : function(displayTarget) {
-			console.log("[controller] menuResetDisplayCheck");
-			console.log(this);
-			console.log(displayTarget);
-//			displayTarget.hide();
-//			if (!this.model.hasChanged()) {
-//				return;
-//			}
-//			for (var key in this.model.changedAttributes()) {
-//				if (this.model.get(key) != this.model.previous(key)) {
-//					displayTarget.fadeIn();	
-//				}
-//			}
+		menuResetDisplayCheck : function(event) {
+			console.debug("[controller] menuResetDisplayCheck");
+			var targetRoot = $(event.currentTarget).closest("[data-id]");
+			var controller = event.data.controller;
+			var uiData = controller.getDataFromTemplate(targetRoot);
+			var model = controller.getSavedModel(targetRoot.attr("data-id"));
+			var resetArea = targetRoot.find(".glyphicon-refresh");
+			controller.isChanged(uiData, model.data) ? resetArea.fadeIn() : resetArea.fadeOut();
+		},
+		/**
+		 *  Template 에서 역으로 데이터를 추출하여 템플릿에 뿌려진 값을 json으로 원복한다.
+		 */
+		getDataFromTemplate : function(target) {
+			console.debug("[controller] getDataFromTemplate");
+			return {
+				asset : { name : target.find("td:eq(0)").text() },
+				entryGroup : { name : target.find("td:eq(1)").text() },
+				amount : target.find("td:eq(2)").text(),
+				date : target.find("td:eq(3)").text(),
+				memo : target.find("td:eq(4)").text()
+			};
+		},
+		isChanged : function(uiData, data) {
+			for (key in uiData) {
+				if (typeof uiData[key] == "object") {
+					var result = this.isChanged(uiData[key], data[key]);
+					if (result) {
+						return result;
+					}
+				} else {
+					if (uiData[key] != data[key]) {
+						return true;
+					}
+				}
+			}
+			return false;
+		},
+		reset : function(event) {
+			console.debug("[controller] reset");
+			var targetRoot = $(event.currentTarget).closest("[data-id]");
+			var controller = event.data.controller;
+			var model = controller.getSavedModel(targetRoot.attr("data-id"));
+			controller.view.render(model);
+		},
+		modify : function(event) {
+			
+		},
+		
+		/** (s) externalEvent **/	
+		meunAddDisplay : function(event) {
+			console.debug("[controller] menuAddDisplay");
+			$(".entry-add-modal").modal();
+		},
+		add : function(event) {
+			console.log("[controller] add");
+			var entry = new Entry({
+				asset : { id : $("[id='asset.id']").val()},
+				entryGroup : {id : $("[id='entryGroup.id']").val()},
+				amount : $("#amount").val(),
+				date : $("#date").val(),
+				memo : $("#memo").val(),
+				transferEntry : $("#transferEntry").is(":checked")
+			});
+			var model = new Model(entry, {controller : event.data.controller});
+			event.data.controller.saveModel(model);
 		}
+		/** (e) externalEvent **/
  	});
 	
 	//var data = new Model({id : 52}, {controller : controller});
--- bluesky-project/webapp/webapp-bluesky/src/main/webapp/public-resources/js/bluesky_data_rest.js
+++ bluesky-project/webapp/webapp-bluesky/src/main/webapp/public-resources/js/bluesky_data_rest.js
@@ -8,11 +8,17 @@ String.prototype.format = function() {
 var View = function(options) {
 	this.template,this.target;
 	this.render = function(model) {
-		console.log("[view] render...");
-		this.target.append($(Mustache.render(this.template, model.data)).attr("data-id", model.getId()));
+		console.debug("[view] render...");
+		var renderedTemplate = $(Mustache.render(this.template, model.data)).attr("data-id", model.getId());
+		var renderTarget = this.target.find("[data-id=" + model.getId() + "]");
+		if (renderTarget.length == 0) {
+			this.target.append(renderedTemplate);
+		} else {
+			renderTarget.replaceWith(renderedTemplate);
+		}
 	};
 	var _initialize = function(obj) {
-		console.log("[view] _initialize...");
+		console.debug("[view] _initialize...");
 		obj.initialize();
 	};
 	this.initialize = function() {};
@@ -29,7 +35,7 @@ var Controller = function(options) {
 	this.url,this.id,this.dataKey,this.view;
 	this.ajax = { dataType : "json", contentType : "application/json" };
 	var _initialize = function(obj) {
-		console.log("[controller] _initialize...");
+		console.debug("[controller] _initialize...");
 		if (!obj.url) {
 			throw new Error("[controller] _initialize error : url empty");
 		}
@@ -41,27 +47,56 @@ var Controller = function(options) {
 	};
 	this.initialize = function() {};
 	this.getUrl = function() {return this.url;};
-	this.events = function() {};
+	this.events = {};
+	this.externalEvents = {};
+	
 	var _initializeEvents = function(obj) {
-		console.log("[controller] _initializeEvents...");
-		for (key in obj.events) {
-			for (event in obj.events[key]) {
-				$(obj.view.target).on(event, key, { controller : obj }, obj[obj.events[key][event]]);
+		console.debug("[controller] _initializeEvents...");
+		var _addEvent = function(obj, target, event, eventMethod) {
+			console.debug("[controller] _addEvent target : %s, event : %s, eventMethod : %s", target, event, eventMethod);
+			$(obj.view.target).on(event, target, { controller : obj }, eventMethod);
+		};
+		for (target in obj.events) {
+			for (event in obj.events[target]) {
+				if (typeof obj.events[target][event] == "object") {
+					for (eventListPart in obj.events[target][event]) {
+						_addEvent(obj, target, event, obj[obj.events[target][event][eventListPart]]);
+					}
+				} else {
+					_addEvent(obj, target, event, obj[obj.events[target][event]]);
+				}
+			}
+		}
+	};
+	var _initializeExternalEvents = function(obj) {
+		console.debug("[controller] _initializeExternalEvents...");
+		var _addEvent = function(obj, target, event, eventMethod) {
+			console.debug("[controller] _addEvent target : %s, event : %s, eventMethod : %s", target, event, eventMethod);
+			$(target).on(event, null, { controller : obj }, eventMethod);
+		};
+		for (target in obj.externalEvents) {
+			for (event in obj.externalEvents[target]) {
+				if (typeof obj.externalEvents[target][event] == "object") {
+					for (eventListPart in obj.externalEvents[target][event]) {
+						_addEvent(obj, target, event, obj[obj.externalEvents[target][event][eventListPart]]);
+					}
+				} else {
+					_addEvent(obj, target, event, obj[obj.externalEvents[target][event]]);
+				}
 			}
 		}
 	};
 	
 	this.getModelList = function() {
 		var obj = this;
-		console.log("[controller] get");
-		console.log("	url : " + this.getUrl());
+		console.debug("[controller] get url : %s", this.getUrl());
 		return $.ajax({
 			url : this.url,
 			type : "get",
 			dataType : this.ajax.dataType,
 			success : function(data) {
 				for (key in data) {
-					obj.setModelData(new Model(data[key], {controller : obj}));
+					obj.setSavedModel(new Model(data[key], {controller : obj}));
 				}
 			}
 		});
@@ -72,60 +107,72 @@ var Controller = function(options) {
 			throw new Error("[controller] get error : id empty");
 		}
 		var obj = this;
-		console.log("[controller] get");
-		console.log("	url : " + this.getUrl() + "/" + model.getId());
+		console.debug("[controller] get url : %s/%s", this.getUrl(), model.getId());
 		return $.ajax({
 			url : this.url + "/" + model.getId(),
 			type : "get",
 			dataType : this.ajax.dataType,
 			success : function(data) {
 				$.extend(model.data, data);
-				obj.setModelData(model);
+				obj.setSavedModel(model);
 			}
 		});
 	};
 	
 	this.saveModel = function(model) {
-		console.log("[controller] save");
+		console.debug("[controller] save");
 		return $.ajax({
 			url : this.url,
 			type : "post",
 			data : JSON.stringify(model.data),
 			dataType : this.ajax.dataType,
 			contentType : this.ajax.contentType,
-			success : function() {
-				console.log("asdg");
+			success : function(data) {
+				$.extend(model.data, data);
+				obj.setSavedModel(model);
 			}
 		});
 	};
 	
 	this.modify = function() {
-		
+		console.debug("[controller] modify");
+		return $.ajax({
+			url : this.url + "/" + model.getId(),
+			type : "put",
+			data : JSON.stringify(model.data),
+			dataType : this.ajax.dataType,
+			contentType : this.ajax.contentType,
+			success : function(data) {
+				$.extend(model.data, data);
+				obj.setSavedModel(model);
+			}
+		});
 	};
 	
 	this.remove = function() {
 		
 	};
 	
-	this.getModelListData = function() {
+	this.getSavedModelList = function() {
 		var list = this.view.target.data(this.dataKey);
 		if (list == null) {
 			list = new Array();
 		}
 		return list;
 	};
-	this.setModelData = function(model) {
-		console.log("[controller] setData");
-		var list = this.getModelListData();
+	this.setSavedModel = function(model) {
+		console.debug("[controller] setData");
+		var list = this.getSavedModelList();
 		list.push(model);
 		this.view.target.data(this.dataKey, list);
 		this.view.render(model);
 		return list;
 	};
-	this.getModelData = function(id) {
-		var list = this.getModelListData();
+	this.getSavedModel = function(id) {
+		var list = this.getSavedModelList();
+		console.debug("[controller] getSavedModel");
 		for (key in list) {
-			if (list[key][this.id] == id) {
+			if (list[key].data[this.id] == id) {
 				return list[key];
 			}
 		}
@@ -134,27 +181,27 @@ var Controller = function(options) {
 	for (key in options) this[key] = options[key];
 	_initialize(this);
 	_initializeEvents(this);
+	_initializeExternalEvents(this);
 };
 
 var Model = function(data, options) {
 	this.data = data;
 	this.controller;
 	this.getId = function() {
-		console.log("[model] getId");
+		console.debug("[model] getId");
 		return this.data[this.controller.id];
 	};
 	this.get = function() {
-		console.log("[model] get -> controller.getModel(model)");
+		console.debug("[model] get -> controller.getModel(model)");
 		this.controller.getModel(this);
 	};
 	this.save = function() {
-		console.log("[model] save -> controller.saveModel(model)");
+		console.debug("[model] save -> controller.saveModel(model)");
 		this.controller.saveModel(this);
 	};
 	var _initialize = function(obj) {
-		console.log("[model] _initialize...");
+		console.debug("[model] _initialize...");
 		obj.initialize();
-		
 		if (!obj.data[obj.controller.id]) obj.data[obj.controller.id] = null;
 	};
 	this.initialize = function() {};
