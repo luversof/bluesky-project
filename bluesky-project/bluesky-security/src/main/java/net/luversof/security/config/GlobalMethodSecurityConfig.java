319ce7e66948a87c57ca61f27892dadfd4ff21be
luversof <luversof@hanmail.net>
2014-02-05 00:31:54 +0900

    jar 판올림, bootstarp 3.0.3 -> 3.1.0

--- bluesky-project/bluesky-security/src/main/java/net/luversof/security/config/GlobalMethodSecurityConfig.java
+++ bluesky-project/bluesky-security/src/main/java/net/luversof/security/config/GlobalMethodSecurityConfig.java
@@ -1,15 +1,15 @@
 package net.luversof.security.config;
 
-import lombok.SneakyThrows;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
+//import lombok.SneakyThrows;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+//import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//
 //@Configuration
 //@EnableGlobalMethodSecurity(prePostEnabled = true)
 //public class GlobalMethodSecurityConfig extends GlobalMethodSecurityConfiguration  {
