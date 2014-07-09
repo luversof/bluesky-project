b339269f56f29960af7d3b45f49563c07b6932a8
luversof <luversof@hanmail.net>
2014-07-10 01:00:45 +0900

    .

---
+++ bluesky-parent/bluesky-webapp/webapp-bluesky/src/main/webapp/public-resources/js/bluesky_bookkeeping_entryForm.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+	var bookkeeping = $.Bookkeeping({
+		url : "/bookkeeping/entry.json",
+		displayArea : $(".bookkeeping-entry-list"),
+		handsontableConfig : {
+				rowHeaders : true,
+				contextMenu : [ "remove_row" ],
+//				dataSchema: {id: null, name: null, userId: null},
+				colHeaders : [ "날짜", "금액", "분류", "메모" ],
+				colWidths : [ 120, 120, 80, 180 ],
+				columnSorting : true,
+				columns : [
+					{ data : "entryDate", readOnly : true },
+					{ data : "amount", readOnly : true , type : "numeric", format : "0,0" },
+					{ data : "entryGroup.name", readOnly : true },
+					{ data : "memo", readOnly : true }
+				],
+		}
+	});
+
+	$(".bookkeeping-entry-load").on("click", function() {
+		bookkeeping.load();
+		$(this).hide();
+	});
+	$(".bookkeeping-entry-load").trigger("click");
+});
