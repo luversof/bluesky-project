89a6440078b9ff15bdce3ad2ae35ab3e0ab351e8
luversof <luversof@hanmail.net>
2015-01-10 03:50:40 +0900

    .

--- bluesky-parent/bluesky-webapp/webapp-bluesky/src/main/java/net/luversof/Application.java
+++ bluesky-parent/bluesky-webapp/webapp-bluesky/src/main/java/net/luversof/Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-
+// 이상하네
     public static void main(String[] args) throws Throwable {
     	SpringApplication springApplication = new SpringApplication(Application.class);
     	springApplication.addInitializers(new BlueskyApplicationContextInitializer());
