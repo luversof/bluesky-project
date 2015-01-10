83d29bec1fbbdcfb7257e62e6aae1fee3b0dbdbd
luversof <luversof@hanmail.net>
2015-01-10 12:25:01 +0900

    develop 수정2

--- bluesky-parent/bluesky-webapp/webapp-bluesky/src/main/java/net/luversof/Application.java
+++ bluesky-parent/bluesky-webapp/webapp-bluesky/src/main/java/net/luversof/Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-// develop
+
     public static void main(String[] args) throws Throwable {
     	SpringApplication springApplication = new SpringApplication(Application.class);
     	springApplication.addInitializers(new BlueskyApplicationContextInitializer());
