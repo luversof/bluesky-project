01522c7a910af8c0a4f21767d18991deaae46f38
luversof <luversof@hanmail.net>
2016-03-29 20:51:33 +0900

    .

--- bluesky-parent/bluesky-test/src/main/java/net/luversof/TestApplication.java
+++ bluesky-parent/bluesky-test/src/main/java/net/luversof/TestApplication.java
@@ -4,11 +4,7 @@ package net.luversof;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
-
 @SpringBootApplication
-@NoArgsConstructor(access=AccessLevel.PRIVATE)
 public class TestApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TestApplication.class, args);
--- bluesky-parent/bluesky-web/bluesky-web-default/src/main/java/net/luversof/Application.java
+++ bluesky-parent/bluesky-web/bluesky-web-default/src/main/java/net/luversof/Application.java
@@ -3,12 +3,9 @@ package net.luversof;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
 import net.luversof.core.context.BlueskyApplicationContextInitializer;
 
 @SpringBootApplication
-@NoArgsConstructor(access=AccessLevel.PRIVATE)
 public class Application {
 
 	public static void main(String[] args) throws Throwable {
