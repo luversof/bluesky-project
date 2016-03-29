c4eb87079a6a9adadede4f252563b19e81ed0e61
bluesky <bluesky@ncsoft.com>
2016-03-29 16:57:55 +0900

    .

--- bluesky-parent/bluesky-test/src/main/java/net/luversof/GeneralTest.java
+++ bluesky-parent/bluesky-test/src/main/java/net/luversof/GeneralTest.java
@@ -3,6 +3,9 @@ package net.luversof;
 import net.luversof.TestApplication;
 import net.luversof.core.context.BlueskyApplicationContextInitializer;
 
+import java.net.Inet4Address;
+import java.net.UnknownHostException;
+
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -10,5 +13,11 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = TestApplication.class, initializers = BlueskyApplicationContextInitializer.class)
 public abstract class GeneralTest {
-
+	public String getRemoteAddr() {
+		try {
+			return Inet4Address.getLocalHost().getHostAddress();
+		} catch (UnknownHostException e) {
+			return "127.0.0.1";
+		}
+	}
 }
