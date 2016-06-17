e6af4e311ec777c8c6f5180e240e1f158b051e61
luversof <luversof@hanmail.net>
2016-06-18 02:32:47 +0900

    .

--- bluesky-parent/bluesky-opensource/bluesky-opensource-mybatis/src/main/java/net/luversof/opensource/mybatis/config/MybatisConfig.java
+++ bluesky-parent/bluesky-opensource/bluesky-opensource-mybatis/src/main/java/net/luversof/opensource/mybatis/config/MybatisConfig.java
@@ -1,13 +1,7 @@
 package net.luversof.opensource.mybatis.config;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.apache.ibatis.type.TypeHandler;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
-
-import net.luversof.opensource.mybatis.type.LocalDateTimeTypeHandler;
 
 public abstract class MybatisConfig {
 	public SqlSessionFactoryBean getSqlSessionFactoryBean() throws Exception {
@@ -15,10 +9,10 @@ public abstract class MybatisConfig {
 		SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
 		sqlSessionFactoryBean.setMapperLocations(
 				new PathMatchingResourcePatternResolver().getResources("classpath*:**/*Mapper.xml"));
-		List<TypeHandler<?>> typeHandlerlist = new ArrayList<>();
-		typeHandlerlist.add(new LocalDateTimeTypeHandler());
-		TypeHandler<?>[] typeHandlers = typeHandlerlist.toArray(new	TypeHandler[typeHandlerlist.size()]);
-		sqlSessionFactoryBean.setTypeHandlers(typeHandlers);
+//		List<TypeHandler<?>> typeHandlerlist = new ArrayList<>();
+//		typeHandlerlist.add(new LocalDateTimeTypeHandler());
+//		TypeHandler<?>[] typeHandlers = typeHandlerlist.toArray(new	TypeHandler[typeHandlerlist.size()]);
+//		sqlSessionFactoryBean.setTypeHandlers(typeHandlers);
 		sqlSessionFactoryBean.setTypeHandlersPackage("net.luversof.opensource.mybatis.type");
 		return sqlSessionFactoryBean;
 	}
