8ac01d980d81c228b0deaba34e5ed85294de3de6
luversof <luversof@hanmail.net>
2016-06-18 02:44:21 +0900

    .

--- bluesky-parent/bluesky-opensource/bluesky-opensource-mybatis/src/main/java/net/luversof/opensource/mybatis/config/MybatisConfig.java
+++ bluesky-parent/bluesky-opensource/bluesky-opensource-mybatis/src/main/java/net/luversof/opensource/mybatis/config/MybatisConfig.java
@@ -1,6 +1,7 @@
 package net.luversof.opensource.mybatis.config;
 
 import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.boot.autoconfigure.SpringBootVFS;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 public abstract class MybatisConfig {
@@ -9,6 +10,7 @@ public abstract class MybatisConfig {
 		SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
 		sqlSessionFactoryBean.setMapperLocations(
 				new PathMatchingResourcePatternResolver().getResources("classpath*:**/*Mapper.xml"));
+		sqlSessionFactoryBean.setVfs(SpringBootVFS.class);
 //		List<TypeHandler<?>> typeHandlerlist = new ArrayList<>();
 //		typeHandlerlist.add(new LocalDateTimeTypeHandler());
 //		TypeHandler<?>[] typeHandlers = typeHandlerlist.toArray(new	TypeHandler[typeHandlerlist.size()]);
