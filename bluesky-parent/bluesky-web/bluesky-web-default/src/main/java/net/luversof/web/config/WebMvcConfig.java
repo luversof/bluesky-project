57c1f4f3fd7308f6e2390ceab1a10329e06a595b
bluesky <bluesky@ncsoft.com>
2016-10-20 09:58:00 +0900

    Java8 Java8TimeDialect 선언제거 (boot 1.4.0 이후 autoconfigure 처리됨)

--- bluesky-parent/bluesky-web/bluesky-web-default/src/main/java/net/luversof/web/config/WebMvcConfig.java
+++ bluesky-parent/bluesky-web/bluesky-web-default/src/main/java/net/luversof/web/config/WebMvcConfig.java
@@ -3,7 +3,6 @@ package net.luversof.web.config;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.format.FormatterRegistry;
@@ -12,7 +11,6 @@ import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
-import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
 import org.thymeleaf.spring4.view.ThymeleafViewResolver;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -56,10 +54,5 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
 	public void configureViewResolvers(ViewResolverRegistry registry) {
 		registry.viewResolver(thymeleafViewResolver);
 		registry.enableContentNegotiation(new MappingJackson2JsonView(objectMapper));
-	}
-
-	@Bean
-	public Java8TimeDialect java8TimeDialect() {
-		return new Java8TimeDialect();
 	}
 }
