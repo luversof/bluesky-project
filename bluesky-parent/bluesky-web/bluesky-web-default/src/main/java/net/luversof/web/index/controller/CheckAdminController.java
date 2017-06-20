153279058576894de5dd7fee62e62faff0985a0b
bluesky <bluesky@ncsoft.com>
2017-06-20 10:03:38 +0900

    .

--- bluesky-parent/bluesky-web/bluesky-web-default/src/main/java/net/luversof/web/index/controller/CheckAdminController.java
+++ bluesky-parent/bluesky-web/bluesky-web-default/src/main/java/net/luversof/web/index/controller/CheckAdminController.java
@@ -1,10 +1,19 @@
 package net.luversof.web.index.controller;
 
+import javax.validation.constraints.NotNull;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import net.luversof.web.constant.AuthorizeRole;
 import net.luversof.web.index.service.MenuService;
 
 @RestController
@@ -17,5 +26,23 @@ public class CheckAdminController {
 	@GetMapping("/menuLoad")
 	public void menuLoad() {
 		menuService.load();
+	}
+	
+
+	@Data
+	@NoArgsConstructor
+	@AllArgsConstructor
+	public static class User {
+		@NotNull(groups = CheckName.class)
+		private String name;
+		private String type;
+		
+		public static interface CheckName {}
+	}
+
+	@PreAuthorize(AuthorizeRole.PRE_AUTHORIZE_ROLE)
+	@GetMapping("/exceptionOrderTest")
+	public void exceptionOrderTest(@Validated(User.CheckName.class) User user, ModelMap modelMap) {
+		modelMap.addAttribute(user);
 	}
 }
