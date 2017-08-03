3f5e6448e25df671fc9c832d49eb846a9c044361
bluesky <bluesky@bluesky2.korea.ncsoft.corp>
2017-08-03 18:01:05 +0900

    .

--- bluesky-parent/bluesky-app/bluesky-app-bookkeeping/src/main/java/net/luversof/bookkeeping/domain/Bookkeeping.java
+++ bluesky-parent/bluesky-app/bluesky-app-bookkeeping/src/main/java/net/luversof/bookkeeping/domain/Bookkeeping.java
@@ -25,13 +25,14 @@ public class Bookkeeping {
 	@GeneratedValue(generator = "uuid-gen")
 	@GenericGenerator(name = "uuid-gen", strategy = "uuid2")
 	@Column(length = 16)
-	@Min(value = 1, groups = { Bookkeeping.Update.class, Bookkeeping.Delete.class, 
-			Asset.Create.class, Asset.Update.class, Asset.Delete.class, 
-			EntryGroup.Create.class, EntryGroup.Update.class, EntryGroup.Delete.class,
-			Entry.Create.class, Entry.Update.class, Entry.Delete.class,
-			EntrySearchInfo.Select.class, EntrySearchInfo.SelectEntryList.class, 
-			StatisticsSearchInfo.Select.class, StatisticsSearchInfo.SelectEntryList.class 
-			})
+	@Min.List({
+		@Min(value = 1, groups = { Bookkeeping.Update.class, Bookkeeping.Delete.class }),
+		@Min(value = 1, groups = { Asset.Create.class, Asset.Update.class, Asset.Delete.class }),
+		@Min(value = 1, groups = { EntryGroup.Create.class, EntryGroup.Update.class, EntryGroup.Delete.class }),
+		@Min(value = 1, groups = { Entry.Create.class, Entry.Update.class, Entry.Delete.class }),
+		@Min(value = 1, groups = { EntrySearchInfo.Select.class, EntrySearchInfo.SelectEntryList.class }),
+		@Min(value = 1, groups = { StatisticsSearchInfo.Select.class, StatisticsSearchInfo.SelectEntryList.class })
+	})
 	private UUID id;
 
 	@NotEmpty(groups = { Create.class, Update.class })
