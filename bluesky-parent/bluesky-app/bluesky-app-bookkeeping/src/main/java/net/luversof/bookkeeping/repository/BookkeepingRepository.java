944cb4362c559ceaa2194760d9a3a89631453c9c
bluesky <bluesky@bluesky2.korea.ncsoft.corp>
2017-08-28 17:43:09 +0900

    data-rest 제거, 부트 로그 default로 변경 (spring boot 버전 보기 편하도록..)

--- bluesky-parent/bluesky-app/bluesky-app-bookkeeping/src/main/java/net/luversof/bookkeeping/repository/BookkeepingRepository.java
+++ bluesky-parent/bluesky-app/bluesky-app-bookkeeping/src/main/java/net/luversof/bookkeeping/repository/BookkeepingRepository.java
@@ -6,12 +6,10 @@ import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.history.RevisionRepository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.transaction.annotation.Transactional;
 
 import net.luversof.bookkeeping.domain.Bookkeeping;
 
-@RepositoryRestResource(collectionResourceRel = "bookkeeping", path = "bookkeeping")
 @Transactional(readOnly = true)
 public interface BookkeepingRepository extends JpaRepository<Bookkeeping, UUID>, RevisionRepository<Bookkeeping, UUID, Integer> {
 	
