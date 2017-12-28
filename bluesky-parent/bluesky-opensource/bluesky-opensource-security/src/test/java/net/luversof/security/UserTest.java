9ba4e32ad8f9f3e96b86e09435b2c0af3a60fefc
bluesky <bluesky@bluesky2.korea.ncsoft.corp>
2017-12-28 18:17:49 +0900

    oauth2 연동 처리 변경 중간 커밋. 암복호화 관련 기능 확인 필요, oauth2 user 객체 처리 관련 수정 필요

--- bluesky-parent/bluesky-opensource/bluesky-opensource-security/src/test/java/net/luversof/security/UserTest.java
+++ bluesky-parent/bluesky-opensource/bluesky-opensource-security/src/test/java/net/luversof/security/UserTest.java
@@ -8,6 +8,9 @@ import net.luversof.user.service.UserService;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.codec.Hex;
+import org.springframework.security.crypto.encrypt.Encryptors;
+import org.springframework.security.crypto.encrypt.TextEncryptor;
 
 @Slf4j
 public class UserTest extends GeneralTest {
@@ -33,5 +36,13 @@ public class UserTest extends GeneralTest {
 		BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
 		User user = userService.addUser("bluesky", bCryptPasswordEncoder.encode("bluesky"));
 		log.debug("user : {}", user);
+	}
+	
+	@Test
+	public void 암호화테스트() {
+		log.debug("test : {}", Hex.encode("cloud".getBytes()));
+		log.debug("test : {}", Hex.decode("73616c74"));
+		TextEncryptor textEncryptor = Encryptors.text("bluesky", "73616c74");
+		log.debug("encrypt:  {}", textEncryptor.encrypt("테스트 문구입니다."));
 	}
 }
