e6da9e7e2deeb0f6e76c5755d930165f70ccb396
bluesky <bluesky@bluesky2.korea.ncsoft.corp>
2018-03-06 17:28:07 +0900

    spring-context-indexer 제거 처리

--- bluesky-parent/bluesky-web/bluesky-web-default/src/main/java/net/luversof/Application.java
+++ bluesky-parent/bluesky-web/bluesky-web-default/src/main/java/net/luversof/Application.java
@@ -1,16 +1,15 @@
 package net.luversof;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringCloudApplication
+@ComponentScan
 public class Application {
 
 	public static void main(String[] args) throws Throwable {
-		SpringApplication springApplication = new SpringApplication(Application.class);
-		//springApplication.addInitializers(new BlueskyApplicationContextInitializer());
-		springApplication.run(args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
