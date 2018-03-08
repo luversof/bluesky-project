78cdd47e4da74225594414e480f71c403d2fc8ef
bluesky <bluesky@bluesky2.korea.ncsoft.corp>
2018-03-08 17:30:25 +0900

    .

--- bluesky-parent/bluesky-core/src/main/java/net/luversof/core/config/FeignConfig.java
+++ bluesky-parent/bluesky-core/src/main/java/net/luversof/core/config/FeignConfig.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableFeignClients
+@EnableFeignClients(basePackages="net.luversof.core")
 public class FeignConfig {
 
 }
--- bluesky-parent/bluesky-core/src/test/java/net/luversof/core/FeignTestService.java
+++ bluesky-parent/bluesky-core/src/test/java/net/luversof/core/FeignTestService.java
@@ -2,14 +2,11 @@ package net.luversof.core;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient("bluesky-ribbon-client")
+@FeignClient("bluesky-web-default")
 public interface FeignTestService {
 
-	@RequestMapping(value = "/_check/discoveryClientServiceList", method = RequestMethod.GET)
+	@GetMapping("/_check/discoveryClientServiceList")
 	String test();
-		
 	
 }
