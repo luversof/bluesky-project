cd7e6fa7a6d8b1f7187ed3634365c79b458a8f23
bluesky <bluesky@bluesky2.korea.ncsoft.corp>
2018-03-12 09:57:41 +0900

    webjars 링크 변수 관리 제거 (webjars-locator-core 사용으로 중간 맵핑 필요없음 판단)

--- bluesky-parent/bluesky-web/bluesky-web-default/src/main/java/net/luversof/web/config/WebConfig.java
+++ bluesky-parent/bluesky-web/bluesky-web-default/src/main/java/net/luversof/web/config/WebConfig.java
@@ -8,9 +8,15 @@ import org.springframework.web.servlet.theme.CookieThemeResolver;
 @Configuration
 public class WebConfig {
 
+	/**
+	 * spring의 ThemeResolver는 모든 properties를 불러올수 있으므로 실제 사용시엔 제약사항을 두는게 좋을 듯
+	 * @return
+	 */
 	@Bean
 	public ThemeResolver themeResolver() {
-		return new CookieThemeResolver();
+		CookieThemeResolver cookieThemeResolver = new CookieThemeResolver();
+		cookieThemeResolver.setDefaultThemeName("theme-default");
+		return cookieThemeResolver;
 	}
 
 }
