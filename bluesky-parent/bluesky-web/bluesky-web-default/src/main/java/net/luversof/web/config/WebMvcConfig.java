48e7200e0d2c401a27404cab9f75f3759a921692
bluesky <luversof@hanmail.net>
2018-07-23 16:30:49 +0900

    thymeleaf decoupledtemplatelogic 설정 제거

--- bluesky-parent/bluesky-web/bluesky-web-default/src/main/java/net/luversof/web/config/WebMvcConfig.java
+++ bluesky-parent/bluesky-web/bluesky-web-default/src/main/java/net/luversof/web/config/WebMvcConfig.java
@@ -1,7 +1,5 @@
 package net.luversof.web.config;
 
-import javax.annotation.PostConstruct;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
@@ -11,21 +9,12 @@ import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.theme.ThemeChangeInterceptor;
 import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
-import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
-
-	@Autowired
-	private SpringResourceTemplateResolver defaultTemplateResolver;
-	
-	@PostConstruct
-	public void postConstruct() {
-		defaultTemplateResolver.setUseDecoupledLogic(true);
-	}
 
 	@Autowired
 	private ThymeleafViewResolver thymeleafViewResolver;
