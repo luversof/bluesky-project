59cccc168194c39a8df131368befe6f42211f242
bluesky <bluesky@nc.com>
2018-10-02 16:04:09 +0900

    .

--- bluesky-parent/bluesky-app/bluesky-app-blog/src/main/java/net/luversof/blog/controller/BlogRepositoryRestController.java
+++ bluesky-parent/bluesky-app/bluesky-app-blog/src/main/java/net/luversof/blog/controller/BlogRepositoryRestController.java
@@ -4,9 +4,7 @@ import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
-import org.springframework.hateoas.Link;
 import org.springframework.hateoas.Resource;
-import org.springframework.hateoas.Resources;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -21,17 +19,15 @@ public class BlogRepositoryRestController {
 	@Autowired
 	private BlogRepository blogRepository;
 
-/*	@GetMapping(value = "/blogs/search/myBlog")
+	@GetMapping(value = "/blogs/search/myBlog")
 	public @ResponseBody ResponseEntity<?> getProducers() {
 		UUID userId = BlogRequestAttributeUtil.getUserId();
 		if (userId == null) {
 			return null;
 		}
 		Blog blog = blogRepository.findByUserId(userId).orElse(null);
-		Resource<Blog> resources = new Resource<String>(blog, linkTo(methodOn(ScannerController.class).getProducers()).withSelfRel()));
-		
-		
-		return ResponseEntity.ok(body)
-	}*/
+		Resource<Blog> resources = new Resource<Blog>(blog);
+		return ResponseEntity.ok(resources);
+	}
 
 }
