c223166c5a8ca24ffe461439b1494e36293afcaf
luversof <luversof@hanmail.net>
2023-01-02 07:40:27 +0900

    api-bookkeeping swagger 주소 변경

--- bluesky-parent/bluesky-web/bluesky-web-swagger-ui/src/main/resources/static/swagger-ui/swagger-initializer.js
+++ bluesky-parent/bluesky-web/bluesky-web-swagger-ui/src/main/resources/static/swagger-ui/swagger-initializer.js
@@ -4,7 +4,7 @@ window.onload = function() {
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
     urls: [
-		{"name" : "bluesky-api-bookkeeping", "url" : "http://localhost:30130/v3/api-docs"}
+		{"name" : "bluesky-api-bookkeeping", "url" : "http://bookkeeping.api.bluesky.local/v3/api-docs"}
 		],
 	operationsSorter : "alpha",
 	withCredentials : true,
