557278a1399e9cd40d465b2221b6a45590287caf
bluesky <bluesky@ncsoft.com>
2023-01-11 13:54:42 +0900

    user api webmvcconfig configuration 설정 추가

--- bluesky-parent/bluesky-api/bluesky-api-user/src/main/java/net/luversof/api/user/config/WebMvcConfig.java
+++ bluesky-parent/bluesky-api/bluesky-api-user/src/main/java/net/luversof/api/user/config/WebMvcConfig.java
@@ -1,8 +1,10 @@
 package net.luversof.api.user.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 	
 	@Override
