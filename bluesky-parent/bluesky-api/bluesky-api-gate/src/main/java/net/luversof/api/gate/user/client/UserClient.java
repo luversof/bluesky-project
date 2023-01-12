500338cef4f8a19487fea5b81c57fef1f0f7df82
bluesky <bluesky@ncsoft.com>
2023-01-13 08:12:06 +0900

    FeignClient에 @RequestParam 추가

--- bluesky-parent/bluesky-api/bluesky-api-gate/src/main/java/net/luversof/api/gate/user/client/UserClient.java
+++ bluesky-parent/bluesky-api/bluesky-api-gate/src/main/java/net/luversof/api/gate/user/client/UserClient.java
@@ -4,12 +4,13 @@ import java.util.Optional;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import net.luversof.api.gate.user.domain.User;
 
-@FeignClient(value = "bluesky-api-user", path = "/api/user")
+@FeignClient(value = "bluesky-api-user", path = "/api/user"/* , url="http://user.api.bluesky.local" */)
 public interface UserClient {
 
 	@GetMapping("/search/findByUserId")
-	Optional<User> findByUserId(String userId);
+	Optional<User> findByUserId(@RequestParam String userId);
 }
