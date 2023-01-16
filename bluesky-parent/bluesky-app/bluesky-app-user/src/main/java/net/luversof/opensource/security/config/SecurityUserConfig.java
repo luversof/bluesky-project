31444301ce1aeb9c49434096cbb884cb73b9288f
bluesky <luversof@hanmail.net>
2023-01-16 23:03:32 +0900

    app-user passwordEncoder bean 추가

--- bluesky-parent/bluesky-app/bluesky-app-user/src/main/java/net/luversof/opensource/security/config/SecurityUserConfig.java
+++ bluesky-parent/bluesky-app/bluesky-app-user/src/main/java/net/luversof/opensource/security/config/SecurityUserConfig.java
@@ -5,11 +5,18 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 
 @Configuration
 public class SecurityUserConfig {
+	
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
 	@Bean
 	UserDetailsManager userDetailsManager(@Qualifier("userDataSource") DataSource userDataSource) {
--- bluesky-parent/bluesky-app/bluesky-app-user/src/test/java/net/luversof/user/UserDetailsManagerTest.java
+++ bluesky-parent/bluesky-app/bluesky-app-user/src/test/java/net/luversof/user/UserDetailsManagerTest.java
@@ -1,29 +1,37 @@
 package net.luversof.user;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.UserDetailsManager;
 
 import lombok.extern.slf4j.Slf4j;
 import net.luversof.GeneralTest;
 
 @Slf4j
-public class UserDetailsManagerTest extends GeneralTest {
+class UserDetailsManagerTest extends GeneralTest {
 
 	@Autowired
 	private UserDetailsManager userDetailsManager;
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 	
 	@Test
 	void createUser() {
 		UserDetails user = User.builder()
 				.username("user")
-				.password("{bcrypt}$2a$10$GEfQb7E10fOeFQo2XowAkubxab4XQGKOvO0Vf.zo6HGUPevVA2t2e")
+				.passwordEncoder(passwordEncoder::encode)
+				.password("pass")
 				.roles("USER")
 				.build();
 		userDetailsManager.createUser(user);
+		assertThat(user).isNotNull();
 	}
 	
 	@Test
