25991c39075f364e7e80072d1ccc7f8e6e82c222
bluesky <luversof@hanmail.net>
2023-02-03 17:25:39 +0900

    api-user 테스트 코드 추가

---
+++ bluesky-parent/bluesky-api/bluesky-api-user/src/test/java/net/luversof/api/user/UserDetailsManagerTest.java
@@ -0,0 +1,54 @@
+package net.luversof.api.user;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.UserDetailsManager;
+
+import lombok.extern.slf4j.Slf4j;
+import net.luversof.GeneralWebTest;
+
+@Slf4j
+class UserDetailsManagerTest implements GeneralWebTest {
+
+	@Autowired
+	private UserDetailsManager userDetailsManager;
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
+	@Test
+	void createUser() {
+		UserDetails user = User.builder()
+				.username("user")
+				.passwordEncoder(passwordEncoder::encode)
+				.password("pass")
+				.roles("USER")
+				.build();
+		userDetailsManager.createUser(user);
+		assertThat(user).isNotNull();
+	}
+	
+	@Test
+	void deleteUser() {
+		userDetailsManager.deleteUser("user");
+	}
+	
+	@Test
+	void encryptest() {
+		var encoder = new BCryptPasswordEncoder();
+		log.debug("encode string : {}", encoder.encode("test"));
+		//encoder result : $2a$10$GEfQb7E10fOeFQo2XowAkubxab4XQGKOvO0Vf.zo6HGUPevVA2t2e
+		assertThat(encoder.matches("test", "$2a$10$GEfQb7E10fOeFQo2XowAkubxab4XQGKOvO0Vf.zo6HGUPevVA2t2e")).isTrue();
+		
+		log.debug("encode string : {}", passwordEncoder.encode("test"));
+		//encode string : {bcrypt}$2a$10$Oc60Qx5tpBNfA6du4HMWDeBotdxoln.wTdiowKkbrHFmIo6jIxrIy
+		assertThat(passwordEncoder.matches("test", "{bcrypt}$2a$10$Oc60Qx5tpBNfA6du4HMWDeBotdxoln.wTdiowKkbrHFmIo6jIxrIy")).isTrue();
+	}
+}
