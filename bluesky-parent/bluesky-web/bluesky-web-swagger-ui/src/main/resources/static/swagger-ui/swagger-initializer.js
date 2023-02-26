29ecf8b8819cd39416bfd9857e5c5e6d0402bc71
bluesky <bluesky@ncsoft.com>
2023-02-27 08:18:40 +0900

    swagger-ui url 수정

--- bluesky-parent/bluesky-web/bluesky-web-swagger-ui/src/main/resources/static/swagger-ui/swagger-initializer.js
+++ bluesky-parent/bluesky-web/bluesky-web-swagger-ui/src/main/resources/static/swagger-ui/swagger-initializer.js
@@ -4,7 +4,9 @@ window.onload = function() {
 	// the following lines will be replaced by docker/configurator, when it runs in a docker-container
 	window.ui = SwaggerUIBundle({
 		urls: [
-			{ "name": "bluesky-api-gate", "url": "http://gate.api.bluesky.local/v3/api-docs" },
+			{ "name": "bluesky-web-gate", "url": "http://gate.web.bluesky.local/v3/api-docs" },
+			{ "name": "bluesky-api-blog", "url": "http://blog.api.bluesky.local/v3/api-docs" },
+			{ "name": "bluesky-api-board", "url": "http://board.api.bluesky.local/v3/api-docs" },
 			{ "name": "bluesky-api-bookkeeping", "url": "http://bookkeeping.api.bluesky.local/v3/api-docs" },
 			{ "name": "bluesky-api-user", "url": "http://user.api.bluesky.local/v3/api-docs" }
 		],
