c72e3b659c638d2102f2c22f1da529c3563281e9
bluesky <bluesky@ncsoft.com>
2023-03-17 18:40:06 +0900

    feign-micrometer, Micrometer Feign Builder 적용 (feign tracing 처리)

--- bluesky-parent/bluesky-web/bluesky-web-gate/src/main/java/net/luversof/web/gate/config/GateWebMvcConfig.java
+++ bluesky-parent/bluesky-web/bluesky-web-gate/src/main/java/net/luversof/web/gate/config/GateWebMvcConfig.java
@@ -7,7 +7,10 @@ import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import feign.Feign;
 import feign.RequestInterceptor;
+import feign.micrometer.MicrometerCapability;
+import io.micrometer.core.instrument.MeterRegistry;
 
 @Configuration
 @EnableConfigurationProperties(GateProperties.class)
@@ -28,5 +31,10 @@ public class GateWebMvcConfig implements WebMvcConfigurer {
 	@Bean
 	RequestInterceptor acceptLanguageHeaderRequestInterceptor () {
 		 return restTemplate -> restTemplate.header("Accept-Language", new String[]{ LocaleContextHolder.getLocale().toLanguageTag() });
+	}
+	
+	@Bean
+	Feign.Builder feignBuilder(MeterRegistry meterRegistry) {
+		return Feign.builder().addCapability(new MicrometerCapability(meterRegistry));
 	}
 }
