faa1fd0b5b0a52167f8d28992da1a82ac7cc0631
bluesky <bluesky@ncsoft.com>
2023-08-01 08:21:10 +0900

    @DevCheckController 처리 변경

--- bluesky-parent/bluesky-web/bluesky-web-gate/src/main/java/net/luversof/web/gate/bookkeeping/controller/BookkeepingDevCheckController.java
+++ bluesky-parent/bluesky-web/bluesky-web-gate/src/main/java/net/luversof/web/gate/bookkeeping/controller/BookkeepingDevCheckController.java
@@ -3,25 +3,22 @@ package net.luversof.web.gate.bookkeeping.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 import io.github.luversof.boot.devcheck.annotation.DevCheckController;
 import net.luversof.web.gate.bookkeeping.client.BookkeepingClient;
 import net.luversof.web.gate.bookkeeping.domain.Bookkeeping;
 
 @DevCheckController
-@RestController
-@RequestMapping(value = "${bluesky-boot.dev-check.path-prefix}/bookkeeping", produces = MediaType.APPLICATION_JSON_VALUE)
 public class BookkeepingDevCheckController {
+	
+	private final String pathPrefix = "/bookkeeping";
 
 	@Autowired
 	private BookkeepingClient bookkeepingClient;
 	
-	@GetMapping
+	@GetMapping(pathPrefix + "/findByUserId")
 	public List<Bookkeeping> findByUserId(@RequestParam String userId) {
 		return bookkeepingClient.findByUserId(userId);
 	}
--- bluesky-parent/bluesky-web/bluesky-web-gate/src/main/java/net/luversof/web/gate/index/controller/GateDevCheckController.java
+++ bluesky-parent/bluesky-web/bluesky-web-gate/src/main/java/net/luversof/web/gate/index/controller/GateDevCheckController.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.core.env.Environment;
-import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -17,9 +16,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 import io.github.luversof.boot.context.support.BlueskyReloadableResourceBundleMessageSource;
 import io.github.luversof.boot.devcheck.annotation.DevCheckController;
@@ -29,9 +26,9 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 
 @DevCheckController
-@RestController
-@RequestMapping(value = "${bluesky-boot.dev-check.path-prefix}", produces = MediaType.APPLICATION_JSON_VALUE)
 public class GateDevCheckController {
+	
+	private final String pathPrefix = "/gate";
 	
 	@Autowired
 	private BlueskyReloadableResourceBundleMessageSource messageSource;
@@ -39,13 +36,13 @@ public class GateDevCheckController {
 	@Autowired
 	private Environment environment;
 
-	@GetMapping("/messageSource")
+//	@GetMapping(pathPrefix + "/messageSource")
 	public MessageSource messageSource() {
 		return messageSource;
 	}
 	
 	@DevCheckDescription("다국어 메세지 전체 목록 조회")
-	@GetMapping("/messageSources")
+	@GetMapping(pathPrefix + "/messageSources")
 	public Map<Object, Object> getMessageSources(@RequestParam(required = false) String searchKeyword) {
 		Map<Object, Object> map = new LinkedHashMap<>();
 		List<Object> keyList = messageSource.getProperties().keySet().stream()
