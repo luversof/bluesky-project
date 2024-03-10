a9cbd82974f42838602f6e944a4cb0db50e59189
luversof <luversof@hanmail.net>
2024-03-11 01:01:00 +0900

    Dynamic CRUD pagination htmx trigger 구성 정리

--- bluesky-parent/bluesky-web/bluesky-web-dynamiccrud/src/main/resources/static/js/common.js
+++ bluesky-parent/bluesky-web/bluesky-web-dynamiccrud/src/main/resources/static/js/common.js
@@ -104,7 +104,7 @@ var searchAreaFn = (() => {
 				param.setParam("page", 1);
 				param.setParam(event.target.name, event.target.value);
 			}
-			htmx.trigger("#listArea", "searchListTrigger");
+			htmx.trigger("#listArea", "showListTrigger");
 		}
 	}
 })();
@@ -139,6 +139,12 @@ var listAreaFn = (() => {
 					el.innerHTML = "<svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke-width='1.5' stroke='currentColor' class='w-6 h-6'><path stroke-linecap='round' stroke-linejoin='round' d='M13.19 8.688a4.5 4.5 0 0 1 1.242 7.244l-4.5 4.5a4.5 4.5 0 0 1-6.364-6.364l1.757-1.757m13.35-.622 1.757-1.757a4.5 4.5 0 0 0-6.364-6.364l-4.5 4.5a4.5 4.5 0 0 0 1.242 7.244' /></svg>";
 				});
 				
+				/** (s) navButton 처리 */
+				targetArea.querySelectorAll(".navButton").forEach(el => el.addEventListener("click", (event) => {
+					param.setParam("page", event.target.dataset.page);
+					htmx.trigger("#listArea", "showListTrigger");
+				}));
+				/** (e) navButton 처리 */
 			
 				/** (s) deleteButton 처리 */
 				targetArea.querySelectorAll(".deleteButton").forEach(el => el.addEventListener("click", (event) => this.checkDeleteData(event)));
