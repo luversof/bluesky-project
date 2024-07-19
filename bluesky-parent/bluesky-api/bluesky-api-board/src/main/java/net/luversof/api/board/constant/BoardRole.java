812e6092ff984e9bb48a3b49d677b6004f8c8cae
luversof <luversof@hanmail.net>
2024-07-19 16:04:09 +0900

    .

--- bluesky-parent/bluesky-api/bluesky-api-board/src/main/java/net/luversof/api/board/constant/BoardRole.java
+++ bluesky-parent/bluesky-api/bluesky-api-board/src/main/java/net/luversof/api/board/constant/BoardRole.java
@@ -1,6 +1,21 @@
 package net.luversof.api.board.constant;
 
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * Spring의 Role Hierarchy 구현을 해야 할까?
+ * Spring의 경우 Map<String, Set<GrantedAuthority>> Role이 각각 자신이 허용하는 Authority를 전부 들고 있는 형태임
+ */
+@Getter
+@AllArgsConstructor
 public enum BoardRole {
-	ADMIN,
-	USER
+	ADMIN(List.of("ROLE_ADMIN", "ROLE_USER", "ROLE_GUEST")),
+	USER(List.of("ROLE_USER", "ROLE_GUEST")),
+	GUEST(List.of("ROLE_GUEST"))
+	;
+	
+	List<String> reachableRoleList;
 }
