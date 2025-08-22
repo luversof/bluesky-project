7c75fd85f8b1dd74a5ccd0b4dfb5ff71e23b788b
luversof <luversof@hanmail.net>
2025-08-23 02:46:37 +0900

    Add OpenApiCustomizer to dynamically set server URL in OpenAPI documentation

--- bluesky-parent/bluesky-api/bluesky-api-blog/src/main/java/net/luversof/api/blog/config/BlogSpringDocConfig.java
+++ bluesky-parent/bluesky-api/bluesky-api-blog/src/main/java/net/luversof/api/blog/config/BlogSpringDocConfig.java
@@ -1,12 +1,16 @@
 package net.luversof.api.blog.config;
 
+import org.springdoc.core.customizers.OpenApiCustomizer;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 @ConditionalOnClass(OpenAPI.class)
@@ -17,9 +21,21 @@ import io.swagger.v3.oas.models.OpenAPI;
 			Blog Client API 목록
 		""",
 		version = "0.0.1-SNAPSHOT"
-	),
-	servers = @Server(url = "/", description = "Default Server URL")
+	)
+//	servers = @Server(url = "/", description = "Default Server URL")
 )
 public class BlogSpringDocConfig {
+	
+	@Bean
+	OpenApiCustomizer serverOpenApiCustomizer() {
+		return openApi -> {
+			var request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+			String scheme = request.getScheme(); // http or https
+			String host = request.getServerName(); // e.g. localhost
+			int port = request.getServerPort(); // e.g. 8080
 
+			String url = scheme + "://" + host + (port != 80 && port != 443 ? ":" + port : "");
+			openApi.setServers(java.util.List.of(new Server().url(url)));
+		};
+	}
 }
