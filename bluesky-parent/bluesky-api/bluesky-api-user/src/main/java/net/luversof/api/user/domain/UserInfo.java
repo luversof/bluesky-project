f487fed440c63d3cca3d840a405f8b734baf8f57
luversof <luversof@hanmail.net>
2025-10-17 17:21:36 +0900

    Refactor user module: annotate UserInfo class with @Table for database mapping

--- bluesky-parent/bluesky-api/bluesky-api-user/src/main/java/net/luversof/api/user/domain/UserInfo.java
+++ bluesky-parent/bluesky-api/bluesky-api-user/src/main/java/net/luversof/api/user/domain/UserInfo.java
@@ -4,6 +4,7 @@ import java.util.UUID;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
 
 import lombok.Data;
 
@@ -12,6 +13,7 @@ import lombok.Data;
  */
 
 @Data
+@Table("UserInfo")
 public class UserInfo {
 
 	@Id
