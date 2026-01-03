0da17f950b66aa4e81dc0dfe747efe6c9162434f
luversof <luversof@hanmail.net>
2026-01-04 02:14:16 +0900

    refactor: add spring-cloud-starter-loadbalancer dependency and clean up unused login URL property

--- bluesky-parent/bluesky-web/bluesky-web-gate/src/main/java/net/luversof/web/gate/user/controller/UserController.java
+++ bluesky-parent/bluesky-web/bluesky-web-gate/src/main/java/net/luversof/web/gate/user/controller/UserController.java
@@ -7,7 +7,6 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import net.luversof.client.user.util.UserUtil;
