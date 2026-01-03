fc57d4278a29717f7169139eda98adde9dd35bea
luversof <luversof@hanmail.net>
2026-01-03 14:08:46 +0900

    refactor: remove bluesky-boot-autoconfigure-devcheck dependency from multiple modules and consolidate in bluesky-api-common

--- bluesky-parent/bluesky-web/bluesky-web-common/src/main/java/net/luversof/web/common/config/WebCommonAutoConfiguration.java
+++ bluesky-parent/bluesky-web/bluesky-web-common/src/main/java/net/luversof/web/common/config/WebCommonAutoConfiguration.java
@@ -1,25 +1,10 @@
 package net.luversof.web.common.config;
 
 import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-
-import net.luversof.web.common.controller.WebCommonDevCheckController;
 
 @AutoConfiguration("blueskyWebCommonAutoConfiguration")
 @EnableConfigurationProperties(WebCommonProperties.class)
 public class WebCommonAutoConfiguration {
 
-	@AutoConfiguration
-	@ConditionalOnClass(name = "io.github.luversof.boot.devcheck.annotation.DevCheckController")
-	static class DevCheckControllerConfiguration {
-
-		@Bean
-		WebCommonDevCheckController webCommonDevCheckController() {
-			return new WebCommonDevCheckController();
-		}
-	
-	}
-	
 }
