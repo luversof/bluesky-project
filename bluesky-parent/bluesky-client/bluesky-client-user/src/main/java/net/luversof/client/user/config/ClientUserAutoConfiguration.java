28f1dfcd89115b26ab1dcf72d3c8b29aa5fded0c
luversof <luversof@hanmail.net>
2026-02-09 02:15:02 +0900

    feat: register ForwardedHeaderFilter as a FilterRegistrationBean; set order to highest precedence

--- bluesky-parent/bluesky-client/bluesky-client-user/src/main/java/net/luversof/client/user/config/ClientUserAutoConfiguration.java
+++ bluesky-parent/bluesky-client/bluesky-client-user/src/main/java/net/luversof/client/user/config/ClientUserAutoConfiguration.java
@@ -3,9 +3,11 @@ package net.luversof.client.user.config;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.Ordered;
 import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.web.filter.ForwardedHeaderFilter;
 
@@ -22,8 +24,10 @@ public class ClientUserAutoConfiguration {
 
 	@Bean
 	@ConditionalOnMissingBean
-	ForwardedHeaderFilter forwardedHeaderFilter() {
-		return new ForwardedHeaderFilter();
+	FilterRegistrationBean<ForwardedHeaderFilter> forwardedHeaderFilter() {
+		FilterRegistrationBean<ForwardedHeaderFilter> bean = new FilterRegistrationBean<>(new ForwardedHeaderFilter());
+		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		return bean;
 	}
 
 	@Bean
