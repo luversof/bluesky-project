7b5b0d99d1bdba1883e562674a50e8037521ce9e
luversof <luversof@hanmail.net>
2026-02-08 23:01:37 +0900

    feat: add ForwardedHeaderFilter bean configuration; ensure proper handling of forwarded headers

--- bluesky-parent/bluesky-client/bluesky-client-user/src/main/java/net/luversof/client/user/config/ClientUserAutoConfiguration.java
+++ bluesky-parent/bluesky-client/bluesky-client-user/src/main/java/net/luversof/client/user/config/ClientUserAutoConfiguration.java
@@ -2,10 +2,12 @@ package net.luversof.client.user.config;
 
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
+import org.springframework.web.filter.ForwardedHeaderFilter;
 
 @AutoConfiguration
 @ConditionalOnClass(name = "org.springframework.security.oauth2.client.registration.ClientRegistrationRepository")
@@ -17,6 +19,12 @@ import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepo
 })
 @PropertySource("classpath:clientUser.properties")
 public class ClientUserAutoConfiguration {
+
+	@Bean
+	@ConditionalOnMissingBean
+	ForwardedHeaderFilter forwardedHeaderFilter() {
+		return new ForwardedHeaderFilter();
+	}
 
 	@Bean
 	OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler(
